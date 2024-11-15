c39b64636f19313755565187471a45389de843a4
Dhanush Shetty <133327430+Dhanus45@users.noreply.github.com>
2024-11-15 15:40:12 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import Profile from "./Components/Profile.jsx"
+import Functional from "./Components/FunctionalCounter.jsx"
+import Condtionbal from "./Components/Conditional.jsx"
+import Product from "./Components/Product.jsx"
+import Form from "./Components/Form.jsx"
+import Todo from "./Components/Todo.jsx"
+function App() {
+  return (
+    <div className="App">
+     <Todo/>
+    </div>
+  );
+
+  
+}
+
+export default App;
